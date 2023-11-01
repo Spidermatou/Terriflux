a0cb3f11abdb72dc3e26f832d071b2dea1b7f3e8
FERNANDES Samuel <glazvonkrovi@gmail.com>
2023-11-01 17:17:49 +0100

    Remove useless Facade

--- Terriflux/Programs/View/MainScene.cs
+++ Terriflux/Programs/View/MainScene.cs
@@ -1,6 +1,6 @@
 ﻿using Godot;
 using Terriflux.Programs.Controller;
-using Terriflux.Programs.Facades;
+using Terriflux.Programs.Factories;
 using Terriflux.Programs.GameContext;
 using Terriflux.Programs.Model.Grid;
 using Terriflux.Programs.Model.Round;
@@ -12,13 +12,15 @@ namespace Terriflux.Programs.View
         private const int gameGridSize = 10;
 
         private readonly RoundModel roundModel = new();
-        private GridModel gridModel = new(gameGridSize);
+        private readonly GridModel gridModel = GridFactory.CreateWasteland(gameGridSize);
 
 
         // nodes
         private RoundCounter _roundView;
-        private Marker2D _markGrid;
         private GridView _gridView;
+
+        // markers
+        private Marker2D _markGrid;
 
         private MainScene() : base() { }
 
@@ -26,17 +28,32 @@ namespace Terriflux.Programs.View
         {
             base._Ready();
 
-            // get children
+            /* ************
+             * get children
+             * */
             _markGrid = GetNode<Marker2D>("GridMark");
             _roundView = GetNode<RoundCounter>("RoundCounter");
 
-            TerritoryManagementFacade territoryManagement = new(this);
 
-            territoryManagement.CreateAndConfigGrid(_markGrid.Position, gameGridSize,
-                out gridModel, out this._gridView);
+            /* ************
+             * config grid
+             * */
+            _gridView = GridFactory.CreateView(gridModel);      // create view
+            _gridView.Position = _markGrid.Position;       // place it
+            _gridView.Scale = new Vector2((float)0.5, (float)0.5);       // reduces size scale to be clearly visible
+            gridModel.AddObserver(_gridView);        // add view as observer
+            this.AddChild(_gridView);       // add view to scene
 
-            
-            territoryManagement.ConfigRounds(roundModel, _roundView);
+            /* ************
+             * config the static grid controller
+             * */
+            GridController.SetGridControl(gridModel);   // have to manage this new grid model
+
+            /* ************
+             * config the rounds
+             * */
+            RoundController.SetModel(roundModel);
+            RoundController.SetView(_roundView);
         }
     }
 }
