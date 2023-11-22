c116cfa6aa86ef4df0d8bf3a5d18ae53acb35acf
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-11-22 08:10:14 +0100

    Correct Textbox-event's code

--- Terriflux/Programs/TextBox/text_box_event.cs
+++ Terriflux/Programs/TextBox/text_box_event.cs
@@ -11,30 +11,37 @@ namespace Terriflux.Programs.GameContext.OurPath
 
 		public override void _Ready()
 		{
-				_message = GetNode<Label>("/MarginContainer/MarginContainer/MarginContainer/HBoxContainer/Message");
-				_nom = GetNode<Label>("/MarginContainer/MarginContainer/Nom");
-				_photo = GetNode<Sprite2D>("/MarginContainer/MarginContainer/Photo");
+			_message = GetNode<Label>("MarginContainer/MarginContainer/MarginContainer/HBoxContainer/Message");
+			_nom = GetNode<Label>("MarginContainer/MarginContainer/Nom");
+			_photo = GetNode<Sprite2D>("MarginContainer/MarginContainer/Photo");
 		}	
 
 		public override void _Process(double delta)
 		{
 		}
-		public void setNom(String Nom)
+		public void SetName(String Nom)
 		{
 			_nom.Text = Nom;
 		}
-		public void setMessage(String Message)
+		public void SetMessage(String Message)
 		{
 			_message.Text = Message;
 		}
+
+		public void SetPicture(Texture2D texture)
+		{
+			_photo.Texture = texture;
+		}
+
+
 		public  text_box_event Design()
 		{
 			return (text_box_event)GD.Load<PackedScene>(OurPaths.VIEW_NODES + "text_box_event.tscn")
 				.Instantiate();
 		}
-		private void _on_texture_button_pressed()
+		private void OnExitTextBoxPressed()
 		{
-			this.GetTree().Quit();
+			this.QueueFree();
 		}
 	}
 }
