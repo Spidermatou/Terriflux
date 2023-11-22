1252da42229532009868a9dfdb386b19d9c40d0b
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-11-22 07:56:00 +0100

    Correct Textbox's (normal) code

--- Terriflux/Programs/TextBox/text_box.cs
+++ Terriflux/Programs/TextBox/text_box.cs
@@ -2,38 +2,33 @@ using Godot;
 using System;
 namespace Terriflux.Programs.GameContext.OurPath
 {
+	/// <summary>
+	/// Ne peut être ferme ! Une fois le joueur devant cette boite de dialogue de fin de jeu, il devra 
+	/// appuyer sur le bouton "relancer / quitter le jeu". 
+	/// </summary>
 	public partial class text_box : CanvasLayer
 	{
-	
-			// Called when the node enters the scene tree for the first time.
 		private Label _message;
+
+		/// <summary>
+		/// A eviter !! Utiliser la fonction static Design() ! 
+		/// </summary>
+		public text_box() { }
+
 		public override void _Ready()
 		{
-			_message = GetNode<Label>("/MarginContainer/MarginContainer/HBoxContainer/Label");
-		}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta)
-		{
-		
+			_message = GetNode<Label>("MarginContainer/MarginContainer/HBoxContainer/Label");
 		}
 		
-		public void setMessage(String Message)
+		public void SetMessage(String Message)
 		{
 			_message.Text = Message;
 		}
 		
-		public  text_box Design()
+		public static text_box Design()
 		{
 			return (text_box)GD.Load<PackedScene>(OurPaths.VIEW_NODES + "text_box.tscn")
 				.Instantiate();
-		}
-		private void _on_texture_button_pressed()
-	{
-		
-		this.quit;
-	// Replace with function body.
-		
 		}
 	}
 }
