d9cc976c55f26e1e2bf02b9d16a41c6810f823cd
LE-ROUX AYMERIC <aymeric.le-roux@etu.unistra.fr>
2023-11-27 14:02:54 +0000

    Design de l'inventaire

--- Terriflux/Programs/View/MainScene.cs
+++ Terriflux/Programs/View/MainScene.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using Terriflux.Programs.Controller;
 using Terriflux.Programs.Factories;
 using Terriflux.Programs.GameContext;
@@ -8,56 +8,56 @@ using Terriflux.Programs.Model.Round;
 
 namespace Terriflux.Programs.View
 {
-    public partial class MainScene : Node2D
-    {
-        private const int gameGridSize = 10;
+	public partial class MainScene : Node2D
+	{
+		private const int gameGridSize = 10;
 
-        private readonly RoundModel roundModel = new();
-        private readonly GridModel gridModel = GridFactory.CreateWasteland(gameGridSize);
+		private readonly RoundModel roundModel = new();
+		private readonly GridModel gridModel = GridFactory.CreateWasteland(gameGridSize);
 
 
-        // nodes
-        private RoundCounter _roundView;
-        private GridView _gridView;
-        private Impacts _impactsView;
+		// nodes
+		private RoundCounter _roundView;
+		private GridView _gridView;
+		private Impacts _impactsView;
 
-        // markers
-        private Marker2D _markGrid;
+		// markers
+		private Marker2D _markGrid;
 
-        private MainScene() : base() { }
+		private MainScene() : base() { }
 
-        public override void _Ready()
-        {
-            base._Ready();
+		public override void _Ready()
+		{
+			base._Ready();
 
-            /* ************
-             * get children
-             * */
-            _markGrid = GetNode<Marker2D>("GridMark");
-            _roundView = GetNode<RoundCounter>("RoundCounter");
-            _impactsView = GetNode<Impacts>("Impacts");
+			/* ************
+			 * get children
+			 * */
+			_markGrid = GetNode<Marker2D>("GridMark");
+			_roundView = GetNode<RoundCounter>("RoundCounter");
+			_impactsView = GetNode<Impacts>("Impacts");
 
-            /* ************
-             * config grid
-             * */
-            _gridView = GridFactory.CreateView(gridModel);      // create view
-            _gridView.Position = _markGrid.Position;       // place it
-            _gridView.Scale = new Vector2((float)0.5, (float)0.5);       // reduces size scale to be clearly visible
-            gridModel.AddObserver(_gridView);        // add view as observer
-            AddChild(_gridView);       // add view to scene
+			/* ************
+			 * config grid
+			 * */
+			_gridView = GridFactory.CreateView(gridModel);      // create view
+			_gridView.Position = _markGrid.Position;       // place it
+			_gridView.Scale = new Vector2((float)0.5, (float)0.5);       // reduces size scale to be clearly visible
+			gridModel.AddObserver(_gridView);        // add view as observer
+			AddChild(_gridView);       // add view to scene
 
-            /* ************
-             * config the static grid controller
-             * */
-            GridController.SetGrid(gridModel);   // have to manage this new grid model
-            GridController.SetRoundManager(roundModel);   // have to manage this new round model
-            GridController.SetControledImpacts(_impactsView);   // have to manage this new impacts model
+			/* ************
+			 * config the static grid controller
+			 * */
+			GridController.SetGrid(gridModel);   // have to manage this new grid model
+			GridController.SetRoundManager(roundModel);   // have to manage this new round model
+			GridController.SetControledImpacts(_impactsView);   // have to manage this new impacts model
 
-            /* ************
-             * config the rounds
-             * */
-            RoundController.SetModel(roundModel);
-            RoundController.SetView(_roundView);
-        }
-    }
+			/* ************
+			 * config the rounds
+			 * */
+			RoundController.SetModel(roundModel);
+			RoundController.SetView(_roundView);
+		}
+	}
 }
---
+++ Terriflux/tblInventaire.cs
@@ -0,0 +1,124 @@
+using Godot;
+using System;
+using Terriflux.Programs.Model.Placeables;
+using System.Collections.Generic;
+using Terriflux.Programs.GameContext;
+
+public partial class Inventory : Node2D, IIventory
+{
+	// quantities management
+	private readonly Dictionary<FlowKind, int> quantitiesProduced = new();    // all quantities produced in one turn
+	private readonly Dictionary<FlowKind, int> quantitiesNeeded = new();    // all quantities needed for one turn
+
+	// children
+	private readonly Dictionary<FlowKind, Sprite2D> _arrows = new(); // quantities arrows visible for user
+	private readonly Dictionary<FlowKind, Label> _variationsLabels = new();    // quantities label visible for user
+
+	// static textures
+	private static readonly Texture2D _constArrow = GD.Load<Texture2D>(OurPaths.TEXTURES + "const.png");
+	private static readonly Texture2D _upArrow = GD.Load<Texture2D>(OurPaths.ICONS + "up.png");
+	private static readonly Texture2D _downArrow = GD.Load<Texture2D>(OurPaths.ICONS + "down.png");
+
+	// CONSTRUCT
+	private Inventory() { }
+
+	public static Inventory Design()
+	{
+		return (Inventory)GD.Load<PackedScene>("Nodes/Inventaire.tscn")
+			.Instantiate();
+	}
+
+	public void Add(FlowKind flow, int amount)
+	{
+		quantitiesProduced[flow] += amount;
+
+		// update hud
+		Update(flow);
+	}
+
+	public void Remove(FlowKind flow, int amount)
+	{
+		quantitiesProduced[flow] -= amount;
+
+		// update hud
+		Update(flow);
+	}
+
+	public bool Contains(FlowKind flow)
+	{
+		return quantitiesProduced.ContainsKey(flow);
+	}
+
+	public bool ContainsEnough(FlowKind flow, int amount)
+	{
+		return this.Contains(flow) && this.quantitiesProduced[flow] > amount;
+	}
+
+	private void Update(FlowKind flow)
+	{
+		int variation = this.quantitiesProduced[flow] - this.quantitiesNeeded[flow];
+
+		// overproduction?
+		if (variation > 0)
+		{
+			this._arrows[flow].Texture = _upArrow; // arrow to up
+			this._variationsLabels[flow].Text = "+" + variation;
+			this._variationsLabels[flow].Modulate = new(0, 10, 0); // green
+		}
+		// perfect balance between product and necessity?
+		else if (variation == 0)
+		{
+			this._arrows[flow].Texture = _constArrow; // vertical bar
+			this._variationsLabels[flow].Text = "0";
+			this._variationsLabels[flow].Modulate = new(255, 255, 255); // black
+		}
+		// underproduction?
+		else
+		{
+			this._arrows[flow].Texture = _downArrow; //  arrow to down
+			this._variationsLabels[flow].Text = variation.ToString();
+			this._variationsLabels[flow].Modulate = new(10, 0, 0); // red
+		}
+	}
+
+	// GODOT
+	public override void _Ready()
+	{
+		// add enum et 0
+		foreach (FlowKind flow in Enum.GetValues(typeof(FlowKind)))
+		{
+			this.quantitiesProduced.Add(flow, 0);
+			this.quantitiesNeeded.Add(flow, 0);
+		}
+
+		// add references for arrows
+		this._arrows.Add(FlowKind.ENERGY, GetNode<Sprite2D>("VarEnergie"));
+		this._arrows.Add(FlowKind.RAW_MATERIAL, GetNode<Sprite2D>("VarMatiere"));
+		this._arrows.Add(FlowKind.WATER, GetNode<Sprite2D>("VarEau"));
+		this._arrows.Add(FlowKind.MANUFACTURED_MERCHANDISE, GetNode<Sprite2D>("VarMarchandise"));
+		this._arrows.Add(FlowKind.CEREALS, GetNode<Sprite2D>("VarCereales"));
+		this._arrows.Add(FlowKind.BREAD, GetNode<Sprite2D>("VarPain"));
+
+		// add references for quantities
+		this._variationsLabels.Add(FlowKind.ENERGY, GetNode<Label>("VarNumberEnergie"));
+		this._variationsLabels.Add(FlowKind.RAW_MATERIAL, GetNode<Label>("VarNumberMatiere"));
+		this._variationsLabels.Add(FlowKind.WATER, GetNode<Label>("VarNumberEau"));
+		this._variationsLabels.Add(FlowKind.MANUFACTURED_MERCHANDISE, GetNode<Label>("VarNumberMarchandise"));
+		this._variationsLabels.Add(FlowKind.CEREALS, GetNode<Label>("VarNumberCereales"));
+		this._variationsLabels.Add(FlowKind.BREAD, GetNode<Label>("VarNumberPain"));
+
+		// reset arrows 
+		foreach (Sprite2D arrow in _arrows.Values)
+		{
+			arrow.Texture = _constArrow;
+		}
+
+		// reset labels
+		foreach (Label label in _variationsLabels.Values)
+		{
+			label.Text = "0";
+			label.Modulate = new(255, 255, 255); // black
+		}
+	}
+
+}
