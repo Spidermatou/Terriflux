f900043a8d2acd40d226f2acdf887b01b2c5d17d
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-12-03 20:08:29 +0100

    Add ICell

---
+++ Terriflux/Programs/ICell.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+namespace Terriflux.Programs
+{
+    public interface IAlert
+    {
+        /// <returns>The size of the cell.</returns>
+        Vector2I GetDimensions();
+
+        bool IsSelected();
+
+        /// <summary>
+        /// Starts cell selection feedback.
+        /// </summary>
+        void Select();
+
+        /// <summary>
+        /// Ends cell selection feedback.
+        /// </summary>
+        void Unselect();
+    }
+}
