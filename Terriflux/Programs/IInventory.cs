d6eba2095c609a66af6c47f542cd9b96cb1f6928
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-12-03 20:38:23 +0100

    Add IInventory

---
+++ Terriflux/Programs/IInventory.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Terriflux.Programs
+{
+    public interface IInventory
+    {
+        void Add(FlowKind flow, int amount);
+        void Remove(FlowKind flow, int amount);
+        bool Contains(FlowKind flow);
+        int GetQuantityOf(FlowKind flow);
+    }
+}
