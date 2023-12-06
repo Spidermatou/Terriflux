9af12d63f1471e5507e849a10318e904a0e5cdd0
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-12-06 10:50:48 +0100

    Add Cell

---
+++ Terriflux/Nodes/Cell.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+using Terriflux.Programs;
+
+namespace Terriflux.Programs;
+
+public abstract partial class Cell : RawNode, ICell
+{
+    private static readonly Texture2D DEFAULT_TEXTURE = GD.Load<Texture2D>(PATH_IMAGES + "grass.png");
+    private static readonly Vector2I SIZE = new(128, 128);
+
+    // children
+    private TextureButton body;
+    private Sprite2D selectedMark;
+    private Label nameLabel;
+
+    protected Cell() : base() { }
+    public override void _Ready()
+    {
+        base._Ready();
+        // congig button
+        this.body = GetNode<TextureButton>("Button");
+        this.body.TextureNormal = DEFAULT_TEXTURE;
+        this.body.Show();
+        // config name hud
+        this.nameLabel = GetNode<Label>("Name");
+        this.nameLabel.Text = this.Name;
+        this.nameLabel.Hide();
+        // config selection hud
+        this.selectedMark = GetNode<Sprite2D>("SelectedMark");
+        this.selectedMark.Texture = GD.Load<Texture2D>(PATH_IMAGES + "willchange.png");
+        this.selectedMark.Hide();
+    }
+
+    protected void ChangeSkin(Texture2D texture)
+    {
+        this.body.TextureNormal = texture;
+    }
+
+    public Vector2I GetDimensions()
+    {
+        return SIZE;
+    }
+
+    public bool IsSelected()
+    {
+        return this.selectedMark.Visible;
+    }
+
+    public void Select()
+    {
+        this.selectedMark.Show();
+    }
+
+    public void Unselect()
+    {
+        this.selectedMark.Hide();
+    }
+
+    private void OnMouseAbove() { this.nameLabel.Show(); }
+    private void OnMouseOutside() { this.nameLabel.Hide(); }
+    private void OnPressed() { Select(); }
+}
