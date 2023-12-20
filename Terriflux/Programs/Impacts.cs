1abbe6e2ec39fe64594cba15a1c2aaa35c357c9c
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-12-20 19:51:16 +0100

    Add Impacts node

---
+++ Terriflux/Programs/Impacts.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+using Terriflux.Programs;
+
+public partial class Impacts : RawNode, IImpacts
+{
+    private Gauge _social;
+    private Gauge _economy;
+    private Gauge _ecology;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _social = GetNode<Gauge>("Social");
+        _economy = GetNode<Gauge>("Economy");
+        _ecology = GetNode<Gauge>("Ecology");
+    }
+
+    public double GetEcology()
+    {
+        return _ecology.Get();
+    }
+
+    public double GetEconomy()
+    {
+        return _economy.Get();
+    }
+
+    public double GetSociability()
+    {
+        return _social.Get();
+    }
+
+    public void IncrementsEcology(double add)
+    {
+        _ecology.Increments(add);
+    }
+
+    public void IncrementsEconomy(double add)
+    {
+        _economy.Increments(add);
+    }
+
+    public void IncrementsSocial(double add)
+    {
+        _social.Increments(add);
+    }
+}
