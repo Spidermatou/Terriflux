c6bea82796377e40674e61a58851d800e7fa1487
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-12-20 12:25:17 +0100

    Update Grid

--- Terriflux/Programs/Grid.cs
+++ Terriflux/Programs/Grid.cs
@@ -5,12 +5,39 @@ using Terriflux.Programs;
 
 public partial class Grid : RawNode, IGrid
 {
-    private readonly Vector2I dimensions;
-    private readonly ICell[,] cells;
+    private Vector2I dimensions;
+    private ICell[,] cells;
 
-    public Grid(Vector2I dimensions)
+    /// <summary>
+    /// Create a Grid.
+    /// <br></br>
+    /// NB : Use instantiation via GridBuilder instead.
+    /// </summary>
+    public Grid() : base() 
+    {
+        this.dimensions = new Vector2I(50, 50);    
+        this.cells = new ICell[dimensions.X, dimensions.Y];
+    } 
+
+    public void SetDimensions(Vector2I dimensions)
     {
         this.dimensions = dimensions;
+        this.cells = new ICell[dimensions.X, dimensions.Y];
+    }
+
+    /// <summary>
+    /// Fills the grid with instances of the specified type.
+    /// </summary>
+    /// <typeparam name="T">Class derived from ICell and having a constructor without parameters.</typeparam>
+    public void FillWith<T>() where T : ICell, new()
+    {
+        for (int line = 0; line < this.cells.GetLength(0); line++)
+        {
+            for (int column = 0; column < this.cells.GetLength(1); column++)
+            {
+                cells[line, column] = new T();
+            }
+        }
     }
 
     public override void _Ready()
@@ -18,9 +45,9 @@ public partial class Grid : RawNode, IGrid
         base._Ready();
     }
 
-    public int DistanceBewteen(Vector2I position1, Vector2I position2)  // TODO
+    public Vector2I GetDimensions()
     {
-        throw new NotImplementedException();
+        return this.dimensions;
     }
 
     public ICell[,] GetAll()
@@ -33,22 +60,29 @@ public partial class Grid : RawNode, IGrid
         // secu
         VerifyCoordinates(coordinates);
 
-        return cells[coordinates.X, coordinates.Y];                
-    }
-
-    public Building[] GetInactiveBuildings()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Vector2I GetDimensions()
-    {
-        return this.dimensions;
+        return cells[coordinates.X, coordinates.Y];
     }
 
     public void SetAt(Vector2I coordinates, ICell cell)
     {
+        // secu
+        VerifyCoordinates(coordinates);
+
         cells[coordinates.X, coordinates.Y] = cell;
+    }
+
+    public int DistanceBewteen(Vector2I position1, Vector2I position2)  // TODO
+    {
+        // secu
+        VerifyCoordinates(position1);
+        VerifyCoordinates(position2);
+
+        return 0;        
+    }
+
+    public Building[] GetInactiveBuildings()    // TODO
+    {
+        throw new NotImplementedException();
     }
 
     private void VerifyCoordinates(Vector2 coordinates)
