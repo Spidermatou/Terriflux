214bd905718834a3fc8c65518556ca9f867e812e
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-12-20 15:22:35 +0100

    Update placementlist

--- Terriflux/Programs/PlacementList.cs
+++ Terriflux/Programs/PlacementList.cs
@@ -1,20 +1,32 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using Terriflux.Programs;
 
 public partial class PlacementList : RawNode
 {
-	// children
-	private ItemList _buildingsList;
+    private readonly Texture2D helpIcon;
 
-	public PlacementList() : base() { }
+    private readonly List<Building> drafts;      // to see needs and production 
+
+    // children
+    private ItemList _buildingsList;
+	private ItemList _helpList;
+
+
+    public PlacementList() : base() 
+    {
+        this.drafts = new();
+        this.helpIcon = GD.Load<Texture2D>(PATH_IMAGES + "Help.png"); 
+    }
 
     public override void _Ready()
 	{
-		this._buildingsList = GetNode<ItemList>("BuildingsList");
+        this._buildingsList = GetNode<ItemList>("BuildingsList");
+		this._helpList = GetNode<ItemList>("HelpList");
 
-		// add one model of all type of buildings avaible
-		BuildingFactory buildingFactory = new();
+        // add one model of all type of buildings avaible
+        BuildingFactory buildingFactory = new();
         Add(buildingFactory.CreateEnergySupplier());
 		Add(buildingFactory.CreateShaft());
 		Add(buildingFactory.CreateField());
@@ -25,14 +37,28 @@ public partial class PlacementList : RawNode
 
     public void Add(Building building)
 	{
-		this._buildingsList.AddItem(building.Name, building.GetIcon(), true);
-	}
+        this._buildingsList.AddItem(building.Name, building.GetIcon(), true);
+        this._helpList.AddItem("", helpIcon, true);
+        this.drafts.Add(building);      
+    }
 
-	private void OnItemSelected(int index)	// TODO
+    private void OnItemSelected(int index)	// TODO
 	{
 		// notify
 
 		// reset focus
 		this._buildingsList.DeselectAll();
+    }
+
+    private void OnHelpSelected(int index)
+    {
+        // say the building's manual
+        Alert alert = (Alert) Instantiate("Alert");
+        alert.Position = new Vector2(-100, 350);
+        this.AddChild(alert);
+        alert.Say(this.drafts[index].GetHelp());
+
+        // reset focus
+        this._helpList.DeselectAll();
     }
 }
