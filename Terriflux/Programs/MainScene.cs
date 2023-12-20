166b74e52e3ae38613a789115855c4ae86e4fcee
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2023-12-20 14:01:33 +0100

    Update grid size on mainscene

--- Terriflux/Programs/MainScene.cs
+++ Terriflux/Programs/MainScene.cs
@@ -11,9 +11,18 @@ public partial class MainScene : RawNode
 	public override void _Ready()
 	{
 		base._Ready();
+
 		// create alert
 		alert = (Alert) Instantiate("Alert");
 		this.AddChild(alert);
+
+        // create grid
+        GridBuilder gridBuilder = new();
+        gridBuilder.BuildWasteland(new(7, 7));
+        Grid grid = gridBuilder.GetResult();
+        grid.Position = GetNode<Marker2D>("GridMark").Position;
+        grid.Scale = new Vector2((float)0.8, (float)0.8); 
+        this.AddChild(grid);
 
 
         /////////////// TESTS
@@ -28,9 +37,6 @@ public partial class MainScene : RawNode
 
         GD.Print(this.GetChildren().Count);
 
-        GridBuilder gridBuilder = new();
-        gridBuilder.BuildWasteland(new(8,8));
-        Grid grid = gridBuilder.GetResult();
-        this.AddChild(grid);
+    
     }
 }
